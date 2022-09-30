630afcc71fb54f89eddf0bbe57c718e31196947f
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-30 19:32:48 +1000

    feat: add dialog

--- assets/js/modules/dialog.js
+++ assets/js/modules/dialog.js
@@ -2,5 +2,5 @@ const openDialogButton = document.querySelector('#open-dialog');
 const dialog = document.querySelector('dialog');
 
 openDialogButton.addEventListener('click', () => {
-  dialog.toggleAttribute('open');
+  dialog.showModal();
 });
