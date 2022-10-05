97d1b0ddd7e752b732a5ce1a88a6fd8b9b99afd6
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-05 14:37:00 +1100

    feat: add dialog

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-import './modules/dialog';
-import './modules/home';
+import { googleAnalyticsId } from '@params'; // eslint-disable-line import/no-unresolved
+import './modules/tpl-dialog';
+import './modules/tpl-home';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM loaded');
+  console.log('Google Analytics ID', googleAnalyticsId);
 });
--- assets/js/modules/dialog.js
+++
@@ -1,6 +0,0 @@
-const openDialogButton = document.querySelector('#open-dialog');
-const dialog = document.querySelector('dialog');
-
-openDialogButton.addEventListener('click', () => {
-  dialog.showModal();
-});
